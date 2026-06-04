a062279f50e6d97abf2698c4827851427bdbd1fc
kirill-lysov-debug <kirill-lysov@inbox.ru>
2026-06-05 00:39:28 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,1 +1,91 @@
+class MapLink:
 
+    def __init__(self):
+        self.map = {}
+
+    def addlink(self, shortlink, link):
+        # Обязательная часть (без вариативных пунктов 1 и 2):
+        # Просто сохраняем/перезаписываем ссылку по короткому коду
+        self.map[shortlink] = link
+        print("Добавлено\n")
+
+    def isSLink(self, shortlink):
+        return shortlink in self.map
+
+    def getLink(self, shortlink):
+        return self.map.get(shortlink, None)
+
+    def printShortLinks(self):
+        # Исправлено: выводим не только ключ, а всю пару, как в задании
+        if not self.map:
+            print("Хранилище пусто\n")
+            return
+        for key, value in self.map.items():
+            print(f"{key} -> {value}")
+        print()
+
+    def deleteLink(self, shortlink):
+        """Вариативная часть: пункт 3 (Удаление ссылки)"""
+        if self.isSLink(shortlink):
+            del self.map[shortlink]
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    map = MapLink()
+
+    print(
+        "Выберите, что хотите сделать с ссылками и их короткими кодами\n"
+    )
+    print("1. Добавить ссылку и короткий код на нее\n")
+    print("2. Проверить существует ли короткий код\n")
+    print("3. Получить ссылку по короткому коду\n")
+    print("4. Вывести все короткие коды\n")
+    print("5. Удалить ссылку по короткому коду (Вариативный п.3)\n")
+    print("Все остальные значения - закончить работу с программой\n")
+
+    A = True
+    while A == True:
+        vubor = input("")
+
+        match vubor:
+            case "1":
+                print("Введите длинную ссылку\n")
+                l = input("")
+                print("Введите короткий код\n")
+                sl = input("")
+
+                map.addlink(sl, l)
+
+            case "2":
+                print("Введите короткий код\n")
+                sl = input("")
+                if map.isSLink(sl):
+                    print("Есть в ассортименте\n")
+                else:
+                    print("Нет в наличии\n")
+
+            case "3":
+                print("Введите короткий код\n")
+                sl = input("")
+                l = map.getLink(sl)
+                if l == None:
+                    print("Нет такой\n")
+                else:
+                    print(f"Итог - {l}\n")
+
+            case "4":
+                map.printShortLinks()
+
+            case "5":  # Наша новая кнопка удаления
+                print("Введите короткий код для удаления\n")
+                sl = input("")
+                if map.deleteLink(sl):
+                    print("Успешно удалено\n")
+                else:
+                    print("Такого кода не существовало\n")
+
+            case _:
+                print("До встречи!")
+                A = False
